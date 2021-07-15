3cb42395d1dc275a4abdca078c34a4f14b55cf46
Eduardo Cano <e.maximocano@gmail.com>
2021-07-15 16:16:40 -0300

    add styles folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import styled from 'styled-components'
+import { GlobalStyle } from "./styles/global";
 
-const Title  = styled.h1`
-  font-size: 64px;
-  color: #444;
-`
 export function App() {
   return (
     <div className="App">
-      <Title>
-        Hello World
-        </Title>
+      <h1>Hello world!</h1>
+      <GlobalStyle />
     </div>
   );
 }
