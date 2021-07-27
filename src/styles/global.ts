9517cc1182e2fd374c33289bc82bc06cca486e89
Eduardo Cano <e.maximocano@gmail.com>
2021-07-27 17:00:09 -0300

    add buttons at the modal

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -87,7 +87,7 @@ export const GlobalStyle = createGlobalStyle`
         border: 0;
         background: transparent;
 
-        transition:  filter 0.2s;
+        transition: filter 0.2s;
 
         &:hover {
             filter: brightness(0.8);
